66e149e7a4202ec381e27e07cbd1275ad24e3c50
karye <karye.webb@gmail.com>
2024-04-18 09:27:43 +0200

    Intro till javascript

---
+++ kapitel-7/gridarea/skript.js
@@ -0,0 +1,17 @@
+/* let tal1 = 55;
+let tal2 = 33;
+let summa = tal1 + tal2;
+
+alert(tal1 + " + " + tal2 + " = " + summa); */
+
+/* let fornam = "Karim";
+let efternamn = "Ryde";
+let namn = fornam + " " + efternamn;
+alert(namn); */
+
+let namn = prompt("Vad heter du?");
+if (namn == "admin") {
+    alert("Välkommen tillbaka!")
+} else {
+    alert("Du har inga rättigheter här!")
+}
