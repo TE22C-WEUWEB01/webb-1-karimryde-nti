297792f01d45b83b52f51b1c7ebd322f452c4d96
karye <karye.webb@gmail.com>
2024-05-30 08:44:03 +0200

    Fix

--- kapitel-8/webbplats/skript.js
+++ kapitel-8/webbplats/skript.js
@@ -1,4 +1,4 @@
-document.querySelector('button').addEventListener('click', function() {
+    document.querySelector('button').addEventListener('click', function() {
     document.body.classList.toggle('dark');
     document.querySelector('.kontainer').classList.toggle('dark');
 });
